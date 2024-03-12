79d2fbccfed01f2b462baecebfa238049b708c00
MateuszSlezal <M4teks93@gmail.com>
2024-03-12 17:25:48 +0100

    fix names + add font and breakpoints

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import '../styles/reset.css';
-import '../styles/globalModule.scss';
-import '../styles/variables.scss';
+import '../styles/global.scss';
+import { Poppins } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
