1a685af0feb48f9b85e38d8f07b93edb82ecf460
kq333 <afugowski@yandex.com>
2024-03-13 09:39:25 +0100

    added form component to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Form } from '@/app/components/FormComponent/Form';
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Form />
+    </main>
+  );
 }
