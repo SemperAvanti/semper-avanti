23b9e2064c9a36ded44e4f63025c34999767311c
MateuszSlezal <M4teks93@gmail.com>
2024-03-13 15:56:25 +0100

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <section>
+        <div className="container">
+          <h2 className="section-title">About us</h2>
+          <div className="two-column-grid-container">
+            <div className="description-container">
+              <h1 className="H5">Our mission</h1>
+              <p>Through innovative training initiatives and personalized support, we aim to equip educators with the knowledge, skills, and confidence needed to inspire lifelong learning and academic success in their students. </p>
+            </div>
+            <div className="description-container">
+              <h1 className="H5">Our vision</h1>
+              <p>Through innovative training initiatives and personalized support, we aim to equip educators with the knowledge, skills, and confidence needed to inspire lifelong learning and academic success in their students. </p>
+            </div>
+          </div>
+          <img src="" alt="meeting event" />
+        </div>
+      </section>
+    </main>
+  );
 }
