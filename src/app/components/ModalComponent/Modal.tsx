96916609a8f3d2445eac556456eae603d5e52800
kq333 <afugowski@yandex.com>
2024-03-13 19:43:03 +0100

    added Image from nextjs

--- src/app/components/ModalComponent/Modal.tsx
+++ src/app/components/ModalComponent/Modal.tsx
@@ -1,19 +1,31 @@
-import './Modal.scss'
+import './Modal.scss';
+import Image from 'next/image';
 
 export const Modal = () => {
   return (
     <div className="modal">
       <div className="modal__container">
         <div className="modal__icons-wrapper">
-          <img src="/mdi_tick.svg" alt="icon success" />
-          <img src="/close-bold.svg" alt="close icon" />
+          <Image
+            src="/mdi_tick.svg"
+            alt="icon success"
+            width={64}
+            height={64}
+          ></Image>
+          <Image
+            src="/close-bold.svg"
+            alt="close icon"
+            width={32}
+            height={32}
+          ></Image>
         </div>
 
         <div className="modal__text-container">
           <h2 className="modal__header">Success</h2>
           <p className="modal__info">
-            Email with the training info has been sent to <strong>emai@email.com </strong>
-            <span className='modal__info-sub-text'>successfully.</span>
+            Email with the training info has been sent to{' '}
+            <strong>emai@email.com </strong>
+            <span className="modal__info-sub-text">successfully.</span>
           </p>
         </div>
 
