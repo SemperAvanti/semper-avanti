4cafc8181ed8e3f6c2691787cb854f74e635a813
Palina Apanovich <palina.apanovich@gmail.com>
2024-03-13 15:45:07 +0100

    buttons

---
+++ src/app/components/Button/Button.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import './button.scss';
+
+interface ButtonProps {
+  name: string;
+  variant: 'primary' | 'secondary' | 'deactivated';
+}
+
+const Button: React.FC<ButtonProps> = ({ name, variant }) => {
+  const buttonClass = `button button--${variant}`;
+  return <button className={buttonClass}>{name}</button>;
+};
+
+export default Button;
