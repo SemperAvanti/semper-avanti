5b1fac55546ee05c8de226712340b9ad2767c048
Palina Apanovich <palina.apanovich@gmail.com>
2024-03-21 19:40:26 +0100

    changes in BEM

--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -4,41 +4,39 @@ import './Home.scss';
 
 const Home = () => {
   return (
-    <>
-      <section className="home">
-        <div className="home__left">
-          <h1 className="H1">Empower with AQE Educate.</h1>
-          <div className="blueLine"></div>
-          <p className="description-text ">
-            Your trusted partner in professional development for educators.{' '}
-          </p>
-          <div className="button-desktop">
-            <Button name="Get info package" variant="primary" />
-          </div>
+    <section className="home">
+      <div className="home__title">
+        <h1 className="home__h1">Empower with AQE Educate.</h1>
+        <div className="blueLine"></div>
+        <p className="home__text ">
+          Your trusted partner in professional development for educators.{' '}
+        </p>
+        <div className="home__button--desktop">
+          <Button name="Get info package" variant="primary" />
         </div>
-        <Image
-          src={'/World Map (1).png'}
-          className="home__worldMap"
-          height={602}
-          width={921}
-          alt={'world map'}
-        />
-        <div className="home__links">
-          <a href="#Malta" className="home__item body-text">
-            Malta
-          </a>
-          <a href="#Canary" className="home__item body-text">
-            Canary
-          </a>
-          <a href="#Ireland" className="home__item body-text">
-            Ireland
-          </a>
-        </div>
-        <div className="button-mobile">
-          <Button name="Get info package" variant="secondary" />
-        </div>
-      </section>
-    </>
+      </div>
+      <Image
+        src={'/World Map (1).png'}
+        className="home__worldMap"
+        height={602}
+        width={921}
+        alt={'world map'}
+      />
+      <div className="home__links">
+        <a href="#Malta" className="home__item body-text">
+          Malta
+        </a>
+        <a href="#Canary" className="home__item body-text">
+          Canary
+        </a>
+        <a href="#Ireland" className="home__item body-text">
+          Ireland
+        </a>
+      </div>
+      <div className="home__button--mobile">
+        <Button name="Get info package" variant="secondary" />
+      </div>
+    </section>
   );
 };
 
