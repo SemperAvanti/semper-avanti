2b6cf86fd4f71341378f88cba3e61785481a37af
kq333 <afugowski@yandex.com>
2024-03-21 17:29:34 +0100

    deleted unnecessary code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 'use client';
 
 import { Form } from '@/app/components/FormComponent/Form';
-import { Modal } from '@/app/components/ModalComponent/Modal';
-
 import { SliderComponent } from './components/SliderComponent/SliderComponent';
 import { SectionAboutUs } from '@/components/SectionAbout/SectionAboutUs';
-
-
 import { FAQ } from './components/FAQ/FAQ';
 import { Footer } from './components/Footer/Footer';
 
@@ -14,16 +10,7 @@ export default function Home() {
   return (
     <main>
       <SectionAboutUs />
-      <Form onClose={handleCloseModal} />
-      <Modal
-        sendedEmail={emailAddress}
-        isOpen={isModalOpen}
-        closeModal={closeModal}
-        isEmailSentSuccessfully={isEmailSentSuccessfully}
-      />
-
-
-
+      <Form />
       <SliderComponent partners={false} title="Success stories" />
       <SliderComponent partners={true} title="Our partners" />
       <FAQ />
