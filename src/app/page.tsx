44dfeb2b843e8d9539c74acafee28cf73f6fdcb1
MateuszSlezal <M4teks93@gmail.com>
2024-03-22 17:24:26 +0100

    addtsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,29 +4,25 @@ import { Form } from '@/app/components/FormComponent/Form';
 import { Aqu } from '@/app/components/AquComponent/Aqu';
 import { SliderComponent } from './components/SliderComponent/SliderComponent';
 import { SectionAboutUs } from '@/components/SectionAbout/SectionAboutUs';
-
 import { FAQ } from './components/FAQ/FAQ';
 import { Footer } from './components/Footer/Footer';
 import HomePage from './components/Home/Home';
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Menu />
-      <HomePage />
-      <SectionAboutUs />
-      <Form onClose={handleCloseModal} />
-      <Modal
-        sendedEmail={emailAddress}
-        isOpen={isModalOpen}
-        closeModal={closeModal}
-        isEmailSentSuccessfully={isEmailSentSuccessfully}
-      />
+      <main>
+        <HomePage />
+        <SectionAboutUs />
+        <Aqu />
+        <Form />
+        <SliderComponent partners={false} title="Success stories" />
+        <SliderComponent partners={true} title="Our partners" />
+        <FAQ />
 
-      <SliderComponent partners={false} title="Success stories" />
-      <SliderComponent partners={true} title="Our partners" />
-      <FAQ />
-      <Footer />
-    </main>
+        <Footer />
+      </main>
+    </>
   );
 }
