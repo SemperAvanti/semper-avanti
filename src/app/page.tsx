a421d6fca203b123c7b83279bd41e64dc4e1e2a4
Hubert Klamann <hubert.klamann1@gmail.com>
2024-03-25 01:15:30 +0100

    GalleryComponent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { SliderComponent } from './components/SliderComponent/SliderComponent';
 import { SectionAboutUs } from '@/components/SectionAbout/SectionAboutUs';
 import { FAQ } from './components/FAQ/FAQ';
 import { Footer } from './components/Footer/Footer';
+import Gallery from './components/GalleryComponent/Gallery';
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <main>
         <Aqu />
         <SectionAboutUs />
+        <Gallery />
         <Form />
         <SliderComponent partners={false} title="Success stories" />
         <SliderComponent partners={true} title="Our partners" />
