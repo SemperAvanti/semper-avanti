86aecbbbc7cde3b24e49099aae77af541154bd3c
Daniel <44358467+danon321@users.noreply.github.com>
2024-05-21 13:34:16 +0200

    Vercel didnt see env values

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ dotenv.config();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
+    TEST: process.env.TEST,
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
   },
