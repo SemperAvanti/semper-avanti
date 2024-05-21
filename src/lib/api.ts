49110572ac8b1dd220a34fd74b3c395182a98282
Daniel <44358467+danon321@users.noreply.github.com>
2024-05-21 13:25:21 +0200

    Fix accessToken error

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,12 @@
 'use server';
 import { createClient } from 'contentful';
 
+const CONTENTFUL_ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN;
+const CONTENTFUL_SPACE_ID = process.env.CONTENTFUL_SPACE_ID;
+
 const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID as string,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
+  space: CONTENTFUL_SPACE_ID as string,
+  accessToken: CONTENTFUL_ACCESS_TOKEN as string,
 });
 
 export async function getContent(contentType: string, locale: string) {
